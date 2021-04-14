f72202349377e75d12d0b6e2435c21338a5f464b
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2021-04-14 16:53:33 -0500

    02 create data my sql (#1)
    * 02-Create Data into BLOG DB of My SQL
    Chenge in the user to login to the site
    
    * Created the Entities or tables: Entry, Blog, Tag
    Those are the new files after added this three Entities.
    note: liquibase:
     # Add 'faker' if you want the sample data to be loaded automatically
     contexts: dev, faker

--- sql/1-createDB.sql
+++
@@ -1,4 +0,0 @@
-
-create database blog; -- Creates the new database
-create user 'jhipuser'@'%' identified by '*LJq*2dAN1'; -- Creates the user
-grant all on blog.* to 'jhipuser'@'%'; -- Gives all privileges to the new user on the newly created database
--- sql/2-security.sql
+++
@@ -1,2 +0,0 @@
-revoke all on blog.* from 'jhipuser'@'%';
-grant select, insert, delete, update on blog.* to 'jhipuser'@'%';
